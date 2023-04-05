0a4a5adf3c3034f522c0d9d9813137510137feed
BirkasTivadar <birkastivadar@gmail.com>
2023-04-05 20:45:17 +0200

    13. PLotting separate plots second video

--- plotting.py
+++ plotting.py
@@ -7,15 +7,22 @@ myCubic = []
 myExponential = []
 
 for i in range(30):
-    mySamples.append(i);
-    myLinear.append(i);
+    mySamples.append(i)
+    myLinear.append(i)
     myQuadratic.append(i ** 2)
     myCubic.append(i ** 3)
     myExponential.append(1.5 ** i)
 
+plt.figure('lin')
 plt.plot(mySamples, myLinear)
+
+plt.figure('quad')
 plt.plot(mySamples, myQuadratic)
+
+plt.figure('cub')
 plt.plot(mySamples, myCubic)
+
+plt.figure('exp')
 plt.plot(mySamples, myExponential)
 
 plt.show()
